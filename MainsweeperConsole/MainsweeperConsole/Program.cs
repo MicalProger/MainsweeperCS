6c54a2650126530d1f01e8d0856bd776a6b80096
Mikhail creator <mmm606@inbox.ru>
2021-08-03 09:49:18 +0300

    Logic update

--- MainsweeperConsole/MainsweeperConsole/Program.cs
+++ MainsweeperConsole/MainsweeperConsole/Program.cs
@@ -13,12 +13,22 @@ namespace MainsweeperConsole
             return Math.Sqrt(Math.Pow(X - point.X, 2) + Math.Pow(Y - point.Y, 2));
         }
 
+        public static Point operator +(Point p1, Point p2)
+        {
+            return new Point(p1.X + p2.X, p1.Y + p2.Y, p1.IsMine && p2.IsMine);
+        }
+
+        public override string ToString()
+        {
+            return $"[{X}, {Y}]";
+        }
+
         public override bool Equals(object obj)
         {
             Point input = obj as Point;
             return input.X == X && input.Y == Y;
         }
-
+        public int MineAround;
         public bool IsMine;
         public int X, Y;
         public Point(int x, int y, bool mine)
@@ -34,7 +44,7 @@ namespace MainsweeperConsole
 
 
         public List<Point> GameField;
-        public Mainsweeper(int xLenght, int yLenght, int minaCount)
+        public Mainsweeper(int xLenght, int yLenght, int minaCount, Point notGenerate)
         {
             double k = 10 / xLenght * yLenght;
             GameField = new List<Point>();
@@ -44,11 +54,38 @@ namespace MainsweeperConsole
             for (int i = 0; i < minaCount; i++)
             {
                 point = new Point(random.Next(0, xLenght), random.Next(0, xLenght), true);
-                while(GameField.Contains(point))
+                while(GameField.Contains(point) || point.Equals(notGenerate))
                     point = new Point(random.Next(0, xLenght), random.Next(0, xLenght), true);
                 GameField.Add(point);
             }
         }
+
+        public Point OpenPoint(Point point)
+        {
+            Point final = new Point(point.X, point.Y, false);
+            if (GameField.Any(k => k.Equals(point) && k.IsMine))
+            {
+                return null;
+            }
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+
+                    if (GameField.Any(k => k.Equals(point + new Point(i, j, false)) && k.IsMine))
+                    {
+                        final.MineAround++;
+                    }
+                }
+            }
+            return final;
+        }
+
+        public void OpenPoints(Point start)
+        {
+
+        }
+
     }
     class Program
     {
